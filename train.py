172bd8c85daeba96a03b955e6c4776a1afd99cca
bubbliiiiing <3323290568@qq.com>
2023-08-27 19:44:41 +0800

    update seed

--- train.py
+++ train.py
@@ -4,6 +4,7 @@
 import datetime
 import os
 import warnings
+from functools import partial
 
 import numpy as np
 import torch
@@ -17,7 +18,8 @@ from nets.efficientdet_training import (FocalLoss, get_lr_scheduler,
                                         set_optimizer_lr)
 from utils.callbacks import EvalCallback, LossHistory
 from utils.dataloader import EfficientdetDataset, efficientdet_dataset_collate
-from utils.utils import download_weights, get_classes, image_sizes, show_config
+from utils.utils import (download_weights, get_classes, image_sizes,
+                         seed_everything, show_config, worker_init_fn)
 from utils.utils_fit import fit_one_epoch
 
 warnings.filterwarnings("ignore")
@@ -44,6 +46,11 @@ if __name__ == "__main__":
     #   没有GPU可以设置成False
     #-------------------------------#
     Cuda            = True
+    #----------------------------------------------#
+    #   Seed    用于固定随机种子
+    #           使得每次独立训练都可以获得一样的结果
+    #----------------------------------------------#
+    seed            = 11
     #---------------------------------------------------------------------#
     #   distributed     用于指定是否使用单机多卡分布式运行
     #                   终端指令仅支持Ubuntu。CUDA_VISIBLE_DEVICES用于在Ubuntu下指定显卡。
@@ -224,6 +231,7 @@ if __name__ == "__main__":
     train_annotation_path   = '2007_train.txt'
     val_annotation_path     = '2007_val.txt'
 
+    seed_everything(seed)
     #------------------------------------------------------#
     #   设置用到的显卡
     #------------------------------------------------------#
@@ -239,6 +247,7 @@ if __name__ == "__main__":
     else:
         device          = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         local_rank      = 0
+        rank            = 0
 
     #----------------------------------------------------#
     #   下载预训练权重
@@ -436,9 +445,11 @@ if __name__ == "__main__":
             shuffle         = True
 
         gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                    drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=train_sampler)
+                                    drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=train_sampler, 
+                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
         gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                    drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=val_sampler)
+                                    drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=val_sampler, 
+                                    worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
 
         #----------------------#
         #   记录eval的map曲线
@@ -486,9 +497,11 @@ if __name__ == "__main__":
                     batch_size = batch_size // ngpus_per_node
 
                 gen             = DataLoader(train_dataset, shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True,
-                                            drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=train_sampler)
+                                            drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=train_sampler, 
+                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
                 gen_val         = DataLoader(val_dataset  , shuffle = shuffle, batch_size = batch_size, num_workers = num_workers, pin_memory=True, 
-                                            drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=val_sampler)
+                                            drop_last=True, collate_fn=efficientdet_dataset_collate, sampler=val_sampler, 
+                                            worker_init_fn=partial(worker_init_fn, rank=rank, seed=seed))
 
                 UnFreeze_flag = True
 
